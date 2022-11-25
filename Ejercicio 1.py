d8d68a3855a575367046e2f1bcf1edade2f1bb89
crltsnch <crltsnch@myuax.com>
2022-11-25 10:51:49 +0100

    constructor

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,4 +1,9 @@
 import heapq #estructura de montículos
 class node:
     def __init__(self, freq, symbol, left=None, right=None):
-        
+        self.freq=freq #frecuencia de cada simbolo
+        self.symbol=symbol #simbolos
+        self.left=left #nodo que sale hacia la izquierda del nodo anterior
+        self.right=right #nodo que sale hacia la derecha del nodo anterior
+
+
