cae79a9f8a86850988911654719b59218eff31d7
crltsnch <crltsnch@myuax.com>
2022-11-25 11:04:03 +0100

    .

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -5,5 +5,7 @@ class node:
         self.symbol=symbol #simbolos
         self.left=left #nodo que sale hacia la izquierda del nodo anterior
         self.right=right #nodo que sale hacia la derecha del nodo anterior
+        self.huff='' #forma de arbol de huffman (0/1)
 
-
+    def __lt__(self, nxt):
+        return self.freq < nxt.freq
